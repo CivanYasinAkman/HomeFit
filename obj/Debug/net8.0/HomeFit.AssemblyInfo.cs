e37695ce5f62d3b2478bdf7915dcccabed60f317
nrsmadmrl <kedilercirkindir@gmail.com>
2026-05-05 14:49:04 +0300

    Add unit tests - 17 tests passing

--- obj/Debug/net8.0/HomeFit.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeFit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ae80ad0e4d34d9acfb88db29d75ce66bf0b0205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a03d32b85aa6873ddd0838cbacd0bcf2190f02")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
