a5cba80ec50ab0d1b1c0f56d91ab425ed85e5673
Civan Yasin Akman <yasin3347577@gmail.com>
2026-05-17 23:55:35 +0300

    mevcut giff ler cloudinary hesabına yüklenerek ordan alınan URL lerle değiştirildi

--- obj/Debug/net8.0/HomeFit.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeFit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb22a22f08ec45deca302839603c235ecf87e9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec77cb3495e43f5835ecca9997150c286c912e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
